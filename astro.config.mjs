bc09fc291a452236b8df9deffdab04f517cbe2ea
SimbaFs <simba.fs@gmail.com>
2025-11-18 20:06:44 +0800

    remove backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import cloudflare from '@astrojs/cloudflare'
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  adapter: cloudflare(),
+	vite: {
+		plugins: [tailwindcss()],
+	},
 })
--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace App {
-	interface Locals {
-		runtime: {
-			env: {
-				SPREADSHEET_CSV_URL: string
-			}
-		}
-	}
-}
